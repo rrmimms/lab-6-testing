fb42b2356d6efc6c7a7a927407d82dc7b1808f4d
dsaito <dsaito@uoregon.edu>
2024-11-08 11:42:21 -0800

    more changes

--- test_bank.py
+++ test_bank.py
@@ -3,24 +3,30 @@ from bank import Account
 
 def test_initial_balance():
     # TODO
-	get_balance(self)
+	_init_(self, "DS", balance=0)
+    self.get_balance()
 
 def test_deposit():
     # TODO
-	deposit(self, 100.0)
+	_init_(self, "DS", balance=0)
+    self.deposit(100.0)
 
 def test_withdraw():
     # TODO
-	withdraw(self,10.0)
+    _init_(self, "DS", balance=0)
+	withdraw(10.0)
 
 def test_deposit_negative_amount():
     # TODO
-	deposit(self, -abs(10))
+    _init_(self, "DS", balance=0)
+	self.deposit(-abs(10))
 
 def test_withdraw_more_than_balance():
     # TODO
-	withdraw(self, get_balance(self) + 100.0)
+    _init_(self, "DS", balance=0)
+	self.withdraw(self.get_balance(self) + 100.0)
 
 def test_withdraw_negative_amount():
     # TODO
-	withdraw(self, -abs(10))
+    _init_(self, "DS", balance=0)
+	self.withdraw(-abs(10))
